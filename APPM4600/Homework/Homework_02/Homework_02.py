9a784df57592c09d15824c1165224548ff369784
magnuswmiller <mgnsmiller@gmail.com>
2025-01-31 18:18:02 -0700

    Question 4 Code.

--- APPM4600/Homework/Homework_02/Homework_02.py
+++ APPM4600/Homework/Homework_02/Homework_02.py
@@ -2,8 +2,9 @@
 
 import math
 import numpy as np
+import matplotlib.pyplot as plt
 
-#code for Question 3
+# Code for Question 3
 x = 9.999999995000000e-10
 
 print(math.e**x-1)
@@ -11,3 +12,49 @@ print(math.e**x-1)
 y = math.e**x
 z = y-1
 print(z)
+
+# Code for Question 4a
+t = np.arange(0, np.pi, np.pi/30)
+y = np.cos(t)
+
+s = np.sum(t*y)
+print("The sum is: %16f"%(s))
+
+# Code for Question 4b
+# Part 1:
+theta = np.linspace(0, 2 * np.pi, 500)
+R = 1.2
+delta_r = 0.1
+f = 15
+p = 0
+
+x = R * (1 + delta_r * np.sin(f * theta + p)) * np.cos(theta)
+y = R * (1 + delta_r * np.sin(f * theta + p)) * np.sin(theta)
+
+plt.figure()
+plt.plot(x, y)
+plt.axis("equal")
+plt.title("Wavy Circle - Single Curve")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.grid(True)
+plt.show()
+
+# Part 2:
+plt.figure()
+for i in range(10):
+    delta_r = 0.05
+    f = 2 + i
+    p = np.random.uniform(0, 2 * np.pi)
+    x = R * (1 + delta_r * np.sin(f * theta + p)) * np.cos(theta)
+    y = R * (1 + delta_r * np.sin(f * theta + p)) * np.sin(theta)
+    plt.plot(x, y, label=f"Curve {i+1}")
+
+plt.axis("equal")
+plt.title("Wavy Circles - Multiple Curves")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.legend()
+plt.grid(True)
+plt.show()
+
